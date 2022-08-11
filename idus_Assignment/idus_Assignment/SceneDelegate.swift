80b4a7575247399f6da181cb6354d995df0fad74
asqw887 <asqw887@gmail.com>
2022-08-11 21:27:31 +0900

    Chore: 프로젝트 초기 셋팅

---
+++ idus_Assignment/idus_Assignment/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  idus_Assignment
+//
+//  Created by 최예주 on 2022/08/11.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        
+        let mainViewController = ViewController()
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+        
+    }
+
+
+
+}
+
---
+++ idus_Assignment/idus_Assignment/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  idus_Assignment
+//
+//  Created by 최예주 on 2022/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
